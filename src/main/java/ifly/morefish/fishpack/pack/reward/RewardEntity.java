537f59620e2a0955727a883503bc4a011eb865a5
miph <libit155@gmail.com>
2023-08-19 19:48:40 +0300

    Remake setup armor system for RewardEntity

--- src/main/java/ifly/morefish/fishpack/pack/reward/RewardEntity.java
+++ src/main/java/ifly/morefish/fishpack/pack/reward/RewardEntity.java
@@ -1,5 +1,6 @@
 package ifly.morefish.fishpack.pack.reward;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
@@ -17,11 +18,7 @@ public class RewardEntity extends RewardAbstract{
 
     List<LivingEntity> livingEntities = new ArrayList<>();
 
-    ItemStack head;
-    ItemStack body;
-    ItemStack leggins;
-    ItemStack boots;
-    //ItemStack[] armors;
+    ItemStack[] armors;
     int amount;
 
     int chance;
@@ -31,7 +28,7 @@ public class RewardEntity extends RewardAbstract{
         entityType = type;
         this.amount = amount;
         this.chance = chance;
-        //armors = new ItemStack[] {head, body, leggins, boots};
+        armors = new ItemStack[4];
     }
 
 
@@ -44,98 +41,67 @@ public class RewardEntity extends RewardAbstract{
         }
     }
 
+	public void setArmor(int ind, ItemStack is)
+	{
+		armors[ind] = is;
+	}
+
+	public ItemStack getArmor(int ind)
+	{
+		return armors[ind];
+	}
+
     public void setArmor(String type, ItemStack is)
     {
-        if(type.endsWith("HELMET")) { head = is; }
-        if(type.endsWith("CHESTPLATE")) { body = is; }
-        if(type.endsWith("LEGGINGS")) { leggins = is; }
-        if(type.endsWith("BOOTS")) { boots = is; }
+        if(type.endsWith("HELMET")) { armors[0] = is; }
+        if(type.endsWith("CHESTPLATE")) { armors[1] = is; }
+        if(type.endsWith("LEGGINGS")) { armors[2] = is; }
+        if(type.endsWith("BOOTS")) { armors[3] = is; }
     }
 
     public ItemStack getArmor(String type)
     {
-        if(type.endsWith("HELMET")) { return head; }
-        if(type.endsWith("CHESTPLATE")) { return body; }
-        if(type.endsWith("LEGGINGS")) { return leggins; }
-        if(type.endsWith("BOOTS")) { return boots; }
+        if(type.endsWith("HELMET")) { return armors[0]; }
+        if(type.endsWith("CHESTPLATE")) { return armors[1]; }
+        if(type.endsWith("LEGGINGS")) { return armors[2]; }
+        if(type.endsWith("BOOTS")) { return armors[3]; }
         return null;
-    }
-
-    public ItemStack getBody() {
-        return body;
-    }
-
-    public ItemStack getHead() {
-        return head;
-    }
-
-    public ItemStack getBoots() {
-        return boots;
-    }
-
-    public ItemStack getLeggins() {
-        return leggins;
     }
 
     public void equip(){
         for (LivingEntity livingEntity: livingEntities){
-            if (getHead() != null){
-                livingEntity.getEquipment().setHelmet(getHead());
+            if (armors[0] != null){
+                livingEntity.getEquipment().setHelmet(armors[0]);
             }
-           if (getBody() != null){
-                livingEntity.getEquipment().setChestplate(getBody());
+           if (armors[1] != null){
+                livingEntity.getEquipment().setChestplate(armors[1]);
            }
-           if (getBoots() != null){
-                livingEntity.getEquipment().setBoots(getBoots());
+           if (armors[2] != null){
+                livingEntity.getEquipment().setBoots(armors[2]);
            }
-           if (getLeggins() != null){
-                livingEntity.getEquipment().setLeggings(getLeggins());
+           if (armors[3] != null){
+                livingEntity.getEquipment().setLeggings(armors[3]);
            }
         }
     }
 
     @Override
     public void Save(YamlConfiguration conf)
-    {
-        int num = confSize(conf);
-        num++;
+	{
+		int num = confSize(conf);
+		num++;
 
-        conf.set("Pack.rewards."+num+".type", "mob");
-        conf.set("Pack.rewards."+num+".entitytype", entityType.name());
-        conf.set("Pack.rewards."+num+".amount", amount);
-        conf.set("Pack.rewards."+num+".chance", chance);
+		conf.set("Pack.rewards." + num + ".type", "mob");
+		conf.set("Pack.rewards." + num + ".entitytype", entityType.name());
+		conf.set("Pack.rewards." + num + ".amount", amount);
+		conf.set("Pack.rewards." + num + ".chance", chance);
 
-        if(head != null)
-        {
-            Map<Enchantment, Integer> enchs = head.getEnchantments();
-            for(Map.Entry<Enchantment, Integer> ench: enchs.entrySet())
-            {
-                conf.set("Pack.rewards."+num+".equipment."+head.getType().name()+".enchants."+ench.getKey().getKey().getKey()+".level", ench.getValue());
-            }
-        }
-        if(body != null)
-        {
-            Map<Enchantment, Integer> enchs = body.getEnchantments();
-            for(Map.Entry<Enchantment, Integer> ench: enchs.entrySet())
-            {
-                conf.set("Pack.rewards."+num+".equipment."+body.getType().name()+".enchants."+ench.getKey().getKey().getKey()+".level", ench.getValue());
-            }
-        }
-        if(leggins != null)
-        {
-            Map<Enchantment, Integer> enchs = leggins.getEnchantments();
-            for(Map.Entry<Enchantment, Integer> ench: enchs.entrySet())
-            {
-                conf.set("Pack.rewards."+num+".equipment."+leggins.getType().name()+".enchants."+ench.getKey().getKey().getKey()+".level", ench.getValue());
-            }
-        }
-        if(boots != null)
-        {
-            Map<Enchantment, Integer> enchs = boots.getEnchantments();
-            for(Map.Entry<Enchantment, Integer> ench: enchs.entrySet())
-            {
-                conf.set("Pack.rewards."+num+".equipment."+boots.getType().name()+".enchants."+ench.getKey().getKey().getKey()+".level", ench.getValue());
-            }
-        }
-    }
+		for(ItemStack armor : armors) {
+		    if(armor == null) { continue; }
+			Map<Enchantment, Integer> enchs = armor.getEnchantments();
+			for(Map.Entry<Enchantment, Integer> ench : enchs.entrySet()) {
+				conf.set("Pack.rewards." + num + ".equipment." + armor.getType().name() + ".enchants." + ench.getKey().getKey().getKey() + ".level", ench.getValue());
+			}
+		}
+	}
 }
